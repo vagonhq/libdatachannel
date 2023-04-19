be59e515b05b11d94b3e7cd273d0eb8072c3b5fe
ognkrmms <12650228+ognkrmms@users.noreply.github.com>
2023-04-19 16:04:19 +0300

    Add server side calculation of received bitrate via TWCC

--- src/rtptwcchandler.cpp
+++ src/rtptwcchandler.cpp
@@ -5,16 +5,19 @@
 #include <cassert>
 namespace rtc {
 
-TwccHandler::TwccHandler(uint8_t extId) {
+TwccHandler::TwccHandler(uint8_t extId, std::shared_ptr<ChainInterop> interop) {
 	assert(extId < 16);
 	twccHeader.preparePacket(extId);
 	twccSeqNum = 0;
+	twccInterop = interop;
 }
 
 ChainedOutgoingProduct TwccHandler::processOutgoingBinaryMessage(ChainedMessagesProduct messages,
                                                                  message_ptr control) {
 	auto outgoing = make_chained_messages_product();
 	outgoing->reserve(messages->size());
+	uint16_t baseSeqNum = twccSeqNum;
+	twccInterop->addFrame(baseSeqNum);
 
 	for (unsigned int i = 0; i < messages->size(); i++) {
 		auto packet = messages->at(i);
@@ -33,6 +36,8 @@ ChainedOutgoingProduct TwccHandler::processOutgoingBinaryMessage(ChainedMessages
 		memcpy(dst, &twccHeader, TWCC_EXT_HEADER_SIZE);
 		dst += TWCC_EXT_HEADER_SIZE;
 		memcpy(dst, src, packet->size() - rtpHeader->getSize());
+
+		twccInterop->addPacketToFrame(baseSeqNum, outgoing->back()->size());
 	}
 
 	return {outgoing, control};
