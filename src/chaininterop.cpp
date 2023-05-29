533626a4c2ebf89b5df01cecfd2093d600158599
ognkrmms <12650228+ognkrmms@users.noreply.github.com>
2023-05-30 02:27:32 +0300

    Fixes recording of arrival times. Use new arrival times in del_var_th calculation. Small change in Kalman filter

--- src/chaininterop.cpp
+++ src/chaininterop.cpp
@@ -1,21 +1,22 @@
 #if RTC_ENABLE_MEDIA
 
 #include "chaininterop.hpp"
+#include <algorithm>
 
 namespace rtc {
 
-
-PacketInfo::PacketInfo(bool isReceived, uint16_t numBytes) : isReceived(isReceived), numBytes(numBytes) {}
+PacketInfo::PacketInfo(bool isReceived, uint16_t numBytes) : isReceived(isReceived), numBytes(numBytes) { arrival_time = 0; }
 
 FrameInfo::FrameInfo(std::chrono::steady_clock::time_point time) : time(time) {}
 
 void FrameInfo::addPacket(uint16_t numBytes) { packets.emplace_back(std::move(PacketInfo(false, numBytes))); }
 
-size_t FrameInfo::updateReceivedStatus(std::vector<bool> statuses, size_t packetStartIdx, size_t statusStartIdx) {
+size_t FrameInfo::updateReceivedStatus(std::vector<bool> statuses, std::vector<double> arrival_times, size_t packetStartIdx, size_t statusStartIdx) {
 	unsigned int bound = packets.size() - packetStartIdx < statuses.size() - statusStartIdx
 	                         ? packets.size() - packetStartIdx : statuses.size() - statusStartIdx;
 	for (unsigned int i = 0; i < bound; i++) {
 		packets.at(i + packetStartIdx).isReceived |= statuses.at(statusStartIdx + i);
+		packets.at(i + packetStartIdx).arrival_time += arrival_times.at(statusStartIdx + i);
 	}
 	return bound;
 }
@@ -24,7 +25,7 @@ size_t FrameInfo::size() const { return packets.size(); }
 
 ReceivedStats FrameInfo::getFrameSizeInBytes() const {
 	ReceivedStats stats = ReceivedStats();
-	for (auto packet : packets) {
+	for (const auto& packet : packets) {
 		if (packet.isReceived){
 			stats.receivedBytes += packet.numBytes;
 			stats.receivedPackets++;
@@ -42,11 +43,21 @@ std::chrono::steady_clock::time_point FrameInfo::getTime() const { return time; 
 
 bool FrameInfo::isFullyReceived() const {
 	bool result = true;
-	for (auto packet : packets) {
-		result = result & packet.isReceived;
+	for (const auto& packet : packets) {
+		result = result && packet.isReceived;
 	}
 	return result;
 }
+
+double FrameInfo::findLastArrivalTime() const {
+	auto result = std::max_element(packets.cbegin(), packets.cend(),
+	                               [](const PacketInfo &a, const PacketInfo &b) -> bool {
+		                               return a.arrival_time < b.arrival_time;
+	                               });
+	return result->arrival_time;
+}
+
+std::chrono::steady_clock::time_point FrameInfo::getDepartureTime() const { return time; }
 
 ChainInterop::ChainInterop(int thresholdMs) { timeThreshold = std::chrono::milliseconds(thresholdMs); }
 
@@ -57,7 +68,7 @@ void ChainInterop::addFrame(uint16_t seqNum) {
 
 void ChainInterop::addPacketToFrame(uint16_t seqNum, uint16_t numBytes) { outgoingFrameInfo.at(seqNum).addPacket(numBytes); }
 
-size_t ChainInterop::updateReceivedStatus(uint16_t baseSeqNum, std::vector<bool> statuses) {
+size_t ChainInterop::updateReceivedStatus(uint16_t baseSeqNum, std::vector<bool> statuses, std::vector<double> arrival_times) {
 	size_t statusStartIdx = 0;
 	uint16_t seqNum = baseSeqNum;
 	size_t processedStatusCount = 0;
@@ -66,8 +77,7 @@ size_t ChainInterop::updateReceivedStatus(uint16_t baseSeqNum, std::vector<bool>
 	if (!outgoingFrameInfo.empty()) {
 		while (statusStartIdx < statuses.size()) {
 			if (outgoingFrameInfo.count(seqNum)) {
-				processedStatusCount =
-				    outgoingFrameInfo.at(seqNum).updateReceivedStatus(statuses, 0, statusStartIdx);
+				processedStatusCount = outgoingFrameInfo.at(seqNum).updateReceivedStatus(statuses, arrival_times, 0, statusStartIdx);
 				seqNum += processedStatusCount;
 				totalProcessedStatusCount += processedStatusCount;
 			} else {
@@ -75,8 +85,7 @@ size_t ChainInterop::updateReceivedStatus(uint16_t baseSeqNum, std::vector<bool>
 				if (iterator != outgoingFrameInfo.end()) {
 					iterator--;
 					if (outgoingFrameInfo.count(iterator->first))
-						processedStatusCount = iterator->second.updateReceivedStatus(
-						    statuses, seqNum - iterator->first, statusStartIdx);
+						processedStatusCount = iterator->second.updateReceivedStatus(statuses, arrival_times, seqNum - iterator->first, statusStartIdx);
 					else
 						break;
 					seqNum += processedStatusCount;
@@ -132,6 +141,23 @@ size_t ChainInterop::sizeReceived() const {
 		nReceived += it->second.isFullyReceived() ? 1 : 0;
 	}
 	return nReceived;
+}
+
+long long ChainInterop::findArrivalIntervalLastTwoFramesMS() const {
+	long long result = 0;
+	auto last_sent = std::max_element(outgoingFrameInfo.begin(), outgoingFrameInfo.end(),
+	                 [](const std::pair<uint16_t, FrameInfo> &a, const std::pair<uint16_t, FrameInfo> &b) -> bool {
+		                 return a.second.getDepartureTime() < b.second.getDepartureTime();
+	                 });
+	if (last_sent != outgoingFrameInfo.begin()) {
+		auto prev = std::prev(last_sent);
+		result = std::chrono::duration_cast<std::chrono::milliseconds>(last_sent->second.getTime() - prev->second.getTime()).count();
+	} else {
+		auto prev = outgoingFrameInfo.rend();
+		result = std::chrono::duration_cast<std::chrono::milliseconds>(last_sent->second.getTime() - prev->second.getTime()).count();
+	}
+
+	return result;
 }
 } // namespace rtc
 
